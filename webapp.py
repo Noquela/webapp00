836277af32f6f1212287a343dc2a577449bb0d34
Noquela <134549537+Noquela@users.noreply.github.com>
2023-11-17 08:38:41 -0300

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,23 +10,20 @@ import seaborn as sns
 import io
 import requests
 
-# Evitar mensagem de depreciação do Pyplot
-if __name__ == '__main__':
-    st.set_option('deprecation.showPyplotGlobalUse', False)
+# Use st.title("") para adicionar um TÍTULO ao seu Web app
+st.title(" ")
 
-    # Use st.title("") para adicionar um TÍTULO ao seu Web app
-    st.title(" ")
+# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
+st.header("Teste de site")
 
-    # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-    st.header("Teste de site")
+# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
+st.subheader("Bem vindos!")
 
-    # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-    st.subheader("Bem vindos!")
+# Use st.write("") para adicionar um texto ao seu Web app
+st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
 
-    # Use st.write("") para adicionar um texto ao seu Web app
-    st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.info("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
 
-    st.info("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.warning("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
 
-    st.warning("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
 
